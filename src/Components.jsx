389e6298d4a40843316f58e00bc193567e08f69b
Noa Nabeshima <noanabeshima@gmail.com>
2023-09-29 20:41:52 -0700

    updated components

--- src/Components.jsx
+++ src/Components.jsx
@@ -4,20 +4,17 @@ import './Tok.css'
 
 
 function Tok({tok, weight}) {
-
     let processedTok = tok.replace(/ /g, '○').replace(/\n/g, '↵');
     const url = window.location.href.split('/').slice(0, -2).join('/') + '/code/' + tok
-
     return (
         <span 
             style={{backgroundColor: `rgba(135,206,250,${Math.min(0.6*weight, 1)})`, border: '0.3px solid black', padding: '0.3px'}}> <a href={url}>{processedTok}</a></span>
     )
 }
 
-function AtomComponent({atom_idx= 1, atom_weight=false}) {
+function AtomComponent({atom_idx, atom_weight=false}) {
     const [data, setData] = useState([['Loading...', 1.008]])
     const [maxWeight, setMaxWeight] = useState(255)
-    const [renderSelf, setRenderSelf] = useState(false)
 
     useEffect(() => {
         const apiURL = "https://tok-embed-nnmf-api-489e56c40ef7.herokuapp.com/atom/"+atom_idx+'?k=400&lowest_ratio=0.18'
@@ -32,8 +29,6 @@ function AtomComponent({atom_idx= 1, atom_weight=false}) {
             .then(data => {
                 setData(data)
                 setMaxWeight(data[0][1])
-                setRenderSelf(true)
-                console.log('Done fetching from: ', apiURL)
             })
             .catch(error => {
                 console.error('There has been a problem with your fetch operation:', error)
@@ -42,9 +37,7 @@ function AtomComponent({atom_idx= 1, atom_weight=false}) {
 
     const weightString = atom_weight ? `, Weight ${atom_weight}` : ''
 
-    if (!renderSelf) {
-        return <></>
-    }
+
     return (
         <>
             <h3 style={{textAlign: 'left'}}>Atom {atom_idx} {weightString}</h3>
@@ -60,46 +53,14 @@ function AtomComponent({atom_idx= 1, atom_weight=false}) {
     )
 }
 
-// function CodeComponent({code_idx=100}) {
-//     const [codeData, setCodeData] = useState([{atom: 'Loading...', weight: 255.}])
-//     const [maxWeight, setMaxWeight] = useState(255)
-
-//     useEffect(() => {
-//         const apiURL = "https://tok-embed-nnmf-api-489e56c40ef7.herokuapp.com/code/"+code_idx
-//         fetch(apiURL)
-//             .then(response => {
-//                 if (!response.ok) {
-//                     throw new Error('Network response was not ok')
-//                 }
-//                 return response.json()
-//             })
-//             .then(data => {
-//                 setCodeData(data)
-//                 setMaxWeight(data[0]['weight'])
-//             })
-//             .catch(error => {
-//                 console.error('There has been a problem with your fetch operation:', error)
-//             })
-//     }, [code_idx])
-
-//     return (
-//         <>
-//             <h2 style={{'textAlign': 'left'}}>Code {code_idx}</h2>
-//             {codeData.map((dict, idx) => {
-//                 return (
-//                     // <span key={idx}>{dict['atom']}</span>
-//                     <WrappedAtomComponent key={idx} atom_idx={dict['atom']} atom_weight={dict['weight']} />
-//                 )
-//             })}
-//         </>
-//     )
-// }
 
 function CodeComponent({codeStringCandidate=' hello'}) {
     const [codeStr, setCodeStr] = useState(codeStringCandidate)
     const [codeIdx, setCodeIdx] = useState('')
     const [codeData, setCodeData] = useState([{atom: 'Loading...', weight: 255.}])
     const [maxWeight, setMaxWeight] = useState(255)
+    const [atomsRendered, setAtomsRendered] = useState(0)
+    const [totalAtoms, setTotalAtoms] = useState(100)
 
     useEffect(() => {
         const apiURL = "https://tok-embed-nnmf-api-489e56c40ef7.herokuapp.com/code_str/"+codeStr
@@ -114,6 +75,8 @@ function CodeComponent({codeStringCandidate=' hello'}) {
                 setCodeStr(data['tok_str'])
                 setCodeIdx(data['tok_id'])
                 setCodeData(data['results'])
+                //length of data['results]
+                setTotalAtoms(data['results'].length)
                 setMaxWeight(data['results'][0][-1])
             })
             .catch(error => {
@@ -121,13 +84,14 @@ function CodeComponent({codeStringCandidate=' hello'}) {
             })
     }, [codeIdx])
 
+
     return (
         <div style={{width: '100%', textAlign: 'left'}}>
-            <h2 style={{'textAlign': 'left'}}>Code {codeIdx}: "{codeStr}"</h2>
+            <h2 style={{'textAlign': 'left'}}>"{codeStr}"</h2>
             {codeData.map((tuple, idx) => {
                 return (
                     // <span key={idx}>{dict['atom']}</span>
-                    <AtomComponent key={idx} atom_idx={tuple[0]} atom_weight={tuple[1]} />
+                    <AtomComponent key={idx} atom_idx={tuple[0]} atom_weight={tuple[1]}/>
                 )
             })}
         </div>
